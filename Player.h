9be74235a73896a09fb13c9806c76e08ea71f410
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-05-10 15:21:41 +0900

    Player.hの変更

--- Player.h
+++ Player.h
@@ -1,1 +1,35 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// 自キャラ
+/// </summary>
+
+class Player {
+public:
+	/// <summary>
+	/// 初期化
+	///  </summary>
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	/// <summary>
+	/// 更新
+	///  </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	///  </summary>
+	void Draw(ViewProjection viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+
+	// モデル
+	Model* model = nullptr;
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
