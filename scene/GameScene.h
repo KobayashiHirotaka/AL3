f876cf11e64b98af0e3bb1aa3940ca6cc54ebe88
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-05-10 15:20:57 +0900

    GameScene.hの変更

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -4,11 +4,11 @@
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
+#include "Player.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -46,32 +46,13 @@ private: // メンバ変数
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 
-		// テクスチャハンドル
 	uint32_t textureHandle_ = 0;
-
-	// スプライト
-	Sprite* sprite_ = nullptr;
-
-	//3Dモデル
 	Model* model_ = nullptr;
 
-	//ワールドトランスフォーム
 	WorldTransform worldTransform_;
-
-	//ビュープロジェクション
 	ViewProjection viewProjection_;
 
-	//サウンドデータハンドル
-	uint32_t soundDateHandle_ = 0;
-
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//ImGuiで値を入力する関数
-	float inputFloat3[3] = {0, 0, 0};
-
-	//デバッグカメラ
-	DebugCamera* debugCamera_ = nullptr;
+	Player* player_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
