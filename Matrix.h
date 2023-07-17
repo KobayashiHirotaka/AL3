bc3a64b5742993c25c5c262f72b03e582e4291e8
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-07-17 10:21:33 +0900

    02_09課題

--- Matrix.h
+++ Matrix.h
@@ -36,5 +36,3 @@ Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
 Vector3 VectorAdd(const Vector3& translation, const Vector3& move);
 
 Vector3 Normalize(const Vector3& v);
-
-Vector3 AdjustVectorLength(const Vector3& vector, float desiredLength);
